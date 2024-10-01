324eeceacded44a85622f247f331b6058f2d2b8b
Sébastien Lotten <sebastienlotten@proton.me>
2024-10-01 18:19:53 +0200

    Mise en place du projet

---
+++ Front-end/Api-Sound-nation/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css'
+
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+      </Routes>
+    </Router>
+  )
+  
+}
+
+export default App 
