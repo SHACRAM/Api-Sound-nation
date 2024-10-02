1dcbd26d6c70241ec00d4e0b1baf1225e32147da
Sébastien Lotten <sebastienlotten@proton.me>
2024-10-02 15:25:45 +0200

    WIP user connection

--- Front-end/Api-Sound-nation/src/App.jsx
+++ Front-end/Api-Sound-nation/src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
+import { ConnectionPage } from './Pages/ConnectionPage'
+
 
 
 function App() {
   return (
     <Router>
       <Routes>
+      <Route path='/' element={<ConnectionPage/>}/>
       </Routes>
     </Router>
   )
