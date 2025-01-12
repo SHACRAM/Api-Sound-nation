d5fc273524fe8f0bab0ed3a67d5ef7ad23ed3d5c
Sébastien Lotten <sebastienlotten@proton.me>
2025-01-12 12:52:50 +0100

    fix bug

--- Back-end/middleware/multerConfig.js
+++ Back-end/middleware/multerConfig.js
@@ -1,24 +1,21 @@
 const multer = require('multer');
-const path = require('path');
 
-// Types MIME autorisés
 const mimeTypes = {
     'image/jpg': 'jpg',
     'image/jpeg': 'jpg',
-    'image/png': 'png',
-};
+    'image/png': 'png'
+}
 
-// Configuration du stockage
 const storage = multer.diskStorage({
-    destination: (req, file, callback) => {
-        const uploadPath = path.join(__dirname, '..', 'uploads');
-        callback(null, uploadPath);
+    destination: (req, file, callback)=>{
+        callback(null, './uploads')
     },
-    filename: (req, file, callback) => {
-        const name = file.originalname.split(' ').join('_'); 
+    filename: (req, file, callback)=>{
+        const name = file.originalname.split('').join('_');
         const extension = mimeTypes[file.mimetype];
-        callback(null, `${name}_${Date.now()}.${extension}`);
-    },
+        callback(null, name + Date.now() + '.' + extension);
+    }
 });
 
-module.exports = multer({ storage: storage });
+
+module.exports = multer({storage: storage});
